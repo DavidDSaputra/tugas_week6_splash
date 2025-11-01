c495008ce1506a9b70523e676e609b4d5a618bc8
DavidDSaputra <davidsaputra2704@gmail.com>
2025-11-02 00:34:39 +0700

    menambahkan wanra biru, merapihkan typografi dan padding input

--- lib/theme.dart
+++ lib/theme.dart
@@ -4,13 +4,14 @@ ThemeData appTheme = ThemeData(
   colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
   useMaterial3: true,
   textTheme: const TextTheme(
-    headlineLarge: TextStyle(fontWeight: FontWeight.w700),
+    headlineLarge: TextStyle(fontWeight: FontWeight.w700, letterSpacing: -0.2),
     titleMedium: TextStyle(fontWeight: FontWeight.w600),
   ),
   inputDecorationTheme: const InputDecorationTheme(
+    contentPadding: EdgeInsets.symmetric(horizontal: 14, vertical: 12),
     border: OutlineInputBorder(),
     focusedBorder: OutlineInputBorder(
-      borderSide: BorderSide(width: 2),
+      borderSide: BorderSide(width: 2, color: Colors.blue),
     ),
   ),
 );
